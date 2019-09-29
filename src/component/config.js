225ae32300cf716f60b834241732dad8de3da88f
Toxiapo <Toxiapo@gmail.com>
2019-09-29 18:04:12 -0400

    update default data

--- src/component/config.js
+++ src/component/config.js
@@ -1,10 +1,7 @@
 export const defaultBoard = ["New", "In Progress", "Done"];
 
 export const defaultTicket = [
-	{ id: 'ticket-1', status: 'New', content: 'Take out the garbage' },
-	{ id: 'ticket-2', status: 'New', content: 'Watch my favorite show' },
-	{ id: 'ticket-3', status: 'Done', content: 'Charge my phone' },
-	{ id: 'ticket-4', status: 'In Progress', content: 'Cook dinner' },
-	{ id: 'ticket-5', status: 'done', content: 'Cook lunch' },
-	{ id: 'ticket-6', status: 'done', content: 'Cook breakfast' },
+	{ id: 'ticket-1', status: 'New', content: 'New Ticket' },
+	{ id: 'ticket-2', status: 'In Progress', content: 'In Progress ticket' },
+	{ id: 'ticket-3', status: 'Done', content: 'done ticket' },
 ]
